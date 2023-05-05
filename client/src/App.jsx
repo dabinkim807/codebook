3d3df4c74d8b1c9dedf9c3af5b01c0527bdf8a92
Dana <dabinkim807@gmail.com>
2023-05-05 19:50:22 -0400

    add working login button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  console.log( window.location.origin)
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
 
 function App() {
+  
 
   return (
     <div className="App">
+      <LoginButton />
     </div>
   )
 }

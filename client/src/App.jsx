d515e5e4099547d25dbb77540d92b36dabd711d5
Dana <dabinkim807@gmail.com>
2023-05-05 20:30:03 -0400

    add logout button and profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,35 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
-
-  console.log( window.location.origin)
   return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button>
+  );
+};
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
 };
 
 function App() {
@@ -16,6 +42,10 @@ function App() {
   return (
     <div className="App">
       <LoginButton />
+      <LogoutButton />
+      <Profile />
+
+     {/* { ? <ValidationPage/> : <SchedulePage/ >} */}
     </div>
   )
 }

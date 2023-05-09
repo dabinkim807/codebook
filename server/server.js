148dce6adcda135e61ce0d883fffdba8e00253c9
Dana <dabinkim807@gmail.com>
2023-05-09 18:09:42 -0400

    integrate auth0 api and add user validation in post users route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const jwtCheck = auth({
   tokenSigningAlg: 'RS256'
 });
 
+const token = process.env.MANAGEMENT_API_ACCESS_TOKEN;
 app.use(cors());
 app.use(express.json());
 // app.use(jwtCheck);   // applies authorization requirement to access all routes; can be applied individually
@@ -70,53 +71,53 @@ app.get('/api/done', jwtCheck, async (req, res) => {
 // MVP - new user sign up + submit codewars username route
 app.post('/api/users', jwtCheck, async (req, res) => {
   try {
-    ///// frontend will send name, id, email, CW username
+    // frontend will send name, id, email, CW username
 
     // ** call Auth0 API
-    // Auth0 return data = {
-    //   given_name: 'Dana', 
-    //   family_name: 'Kim',
-    //   nickname: 'dabinkim807', 
-    //   name: 'Dana Kim', 
-    //   picture: 'https://lh3.googleusercontent.com/a/AGNmyxa2gCzcpNHziuFs8bC0ErSUEttDTPi-x2UZG7Sl-Q=s96-c',
-    //   email: "dabinkim807@gmail.com"
-    //   email_verified: true,
-    //   locale: "en",
-    //   sub: "google-oauth2|115940204927970477883",
-    //   updated_at: "2023-05-08T13:24:52.523Z"
-    // }
+    const params = new URLSearchParams({
+      q: `user_id:"${req.auth.payload.sub}"`,
+      search_engine: 'v3'
+    });
+    const auth_response = await fetch(`https://dev-y8l2e8exqiihl4qw.us.auth0.com/api/v2/users?${params}`, {
+      method: "GET",
+      headers: {
+        "authorization": `BEARER ${token}`
+      }
+    });
+    const auth_data = await auth_response.json();
+    console.log(auth_data);
 
     // ** choose a random code challenge from db
-    // const { rows: random } = await db.query("SELECT * FROM code_challenges WHERE rank = 'Beginner' ORDER BY random() LIMIT 1");
-    // console.log(random);
-    // console.log(random[0].challenge);
+    const { rows: random } = await db.query("SELECT * FROM code_challenges WHERE rank = 'Beginner' ORDER BY random() LIMIT 1");
+    console.log(random);
+    console.log(random[0].challenge);
 
-    // // ** call Codewars List of CC API
-    // const response = await fetch(`https://www.codewars.com/api/v1/users/${users[0].username}/code-challenges/completed`);
-    // const data = await response.json();
-    // if (data.success === false) {
-    //   return res.status(200).json({'validated': false});
-    // }
+    // ** call Codewars List of CC API
+    const cw_response = await fetch(`https://www.codewars.com/api/v1/users/${req.body.username}/code-challenges/completed`);
+    const cw_data = await cw_response.json();
+    if (cw_data.success === false) {
+      return res.status(200).json({'validated': false});
+    }
 
-    // for (const challenge of data.data) {
-    //   if (challenge.id !== random[0].challenge) {
-    //     // on conflict -- user might try to sign in twice without validating
-    //     const { rows: test } = await db.query(
-    //       `
-    //       INSERT INTO users (user_id, username, email, test_challenge, test_created, name) 
-    //       VALUES($1, $2, $3, $4, $5, $6) 
-    //       ON CONFLICT (user_id) DO UPDATE SET username = Excluded.username
-    //       WHERE user_id = $1
-    //       `
-    //     , [req.auth.payload.sub, req.query.user, req.auth.payload.email, challenge.id, NOW(), req.auth.payload.name]);
+    for (const challenge of data.data) {
+      if (challenge.id !== random[0].challenge) {
+        // on conflict -- user might try to sign in twice without validating
+        const { rows: test } = await db.query(
+          `
+          INSERT INTO users (user_id, username, email, test_challenge, test_created, name) 
+          VALUES($1, $2, $3, $4, $5, $6) 
+          ON CONFLICT (user_id) DO UPDATE SET username = Excluded.username
+          WHERE user_id = $1
+          `
+        , [req.auth.payload.sub, req.body.username, auth_data[0].email, challenge.id, NOW(), auth_data[0].name]);
 
-    //     const returnObj = {
-    //       test_challenge: test.rows[0].test_challenge,
-    //       test_created: test.rows[0].test_created,
-    //     }
-    //     return res.status(200).json(returnObj);
-    //   }
-    // }
+        const returnObj = {
+          test_challenge: test.rows[0].test_challenge,
+          test_created: test.rows[0].test_created,
+        }
+        return res.status(200).json(returnObj);
+      }
+    }
 
     res.status(200).json({'validated': false});
   } catch (e) {

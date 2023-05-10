19928690c5c6c61581c7a98668a65b70590f4105
Dana <dabinkim807@gmail.com>
2023-05-09 20:20:49 -0400

    clean up code, still need to test post route

--- server/server.js
+++ server/server.js
@@ -19,32 +19,48 @@ const token = process.env.MANAGEMENT_API_ACCESS_TOKEN;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(REACT_BUILD_DIR));
-// app.use(jwtCheck);   // applies authorization requirement to access all routes; can be applied individually
+// app.use(jwtCheck);   // applies authorization requirement to access all routes; can be applied to individual routes
 
 app.get('/', (req, res) => {
-  // res.send("Hi! This is Dana's Express JS template");
   res.sendFile(path.join(REACT_BUILD_DIR, "index.html"));
 });
 
-// shows "UnauthorizedError: Unauthorized" when logged out and logged in (should only show for logged out)
 app.get('/authorized', jwtCheck, (req, res) => {
+  console.log(req.auth.payload);
   res.send('Secured Resource');
 });
 
-// MVP - log in route; if returning user, will already be in database 
+// this route proves that all req.auth.payload comes from accessToken sent from frontend
+app.get('/unauthorized', (req, res) => {
+  console.log(req.auth);
+  res.send('Unsecured Resource');
+});
+
+// log in route; if returning user, will already be in database 
 app.get('/api/user', jwtCheck, async (req, res) => {
   try {
     const { rows: users } = await db.query("SELECT * FROM users WHERE user_id = $1", [req.auth.payload.sub]);
     res.json(users.length === 1);
+    // user is not in DB
+      // show user Sign Up component
+
+    // user is validated
+      // if true, then show Schedule Page and send frontend: cc_category, cc_rank, cc_frequency, cc_day [fill in / pre-populate Schedule fields]
+    // otherwise, test_challenge has already been assigned
+      // call CW API, check test_challenge is passed
+        // if true, send user to Schedule Page
+        // otherise, has 10 min passed from test_created?
+          // if true, then send user back to Sign Up component
+          // otherwise, show user Validation Page and send test_challenge and/or test_created
+
   } catch (e) {
     return res.status(400).json({ e });
   }
 });
-// MVP - validation route; after post, basic user info is in database
+// validation route; after post, basic user info is in database
 app.get('/api/done', jwtCheck, async (req, res) => {
   try {
     const { rows: users } = await db.query("SELECT * FROM users WHERE user_id = $1", [req.auth.payload.sub]);
-    // test_challenge and test_created will be generated from backend, separate from route
 
     if (users.length !== 1) {
       return res.status(400).send(`No user with user id ${req.auth.payload.sub}`);
@@ -53,9 +69,7 @@ app.get('/api/done', jwtCheck, async (req, res) => {
       return res.status(200).json({'validated': true});
     }
     
-    const url = `https://www.codewars.com/api/v1/users/${users[0].username}/code-challenges/completed`;
-    
-    fetch(url)
+    fetch(`https://www.codewars.com/api/v1/users/${users[0].username}/code-challenges/completed`)
       .then((response) => response.json())
       .then((data) => {
     
@@ -72,12 +86,13 @@ app.get('/api/done', jwtCheck, async (req, res) => {
   }
 });
 
-// MVP - new user sign up + submit codewars username route
+// new user sign up + submit codewars username route
 app.post('/api/users', jwtCheck, async (req, res) => {
   try {
     // frontend will send name, id, email, CW username
 
-    // ** call Auth0 API
+    // call Auth0 API
+    // jwtCheck checks the accessToken that the user passes to backend thru frontend request via getAccessTokensSilently and provides the user data automatically
     const params = new URLSearchParams({
       q: `user_id:"${req.auth.payload.sub}"`,
       search_engine: 'v3'
@@ -91,12 +106,13 @@ app.post('/api/users', jwtCheck, async (req, res) => {
     const auth_data = await auth_response.json();
     console.log(auth_data);
 
-    // ** choose a random code challenge from db
+    // choose a random code challenge from db
+    // after adding more hardcoded code challenges to db, increase limit to ~5
     const { rows: random } = await db.query("SELECT * FROM code_challenges WHERE rank = 'Beginner' ORDER BY random() LIMIT 1");
     console.log(random);
     console.log(random[0].challenge);
 
-    // ** call Codewars List of CC API
+    // call Codewars List of CC API
     const cw_response = await fetch(`https://www.codewars.com/api/v1/users/${req.body.username}/code-challenges/completed`);
     const cw_data = await cw_response.json();
     if (cw_data.success === false) {

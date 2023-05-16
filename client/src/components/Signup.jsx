010132fcac885895296b0dfd220aaab1b281c4bf
Dana <dabinkim807@gmail.com>
2023-05-16 17:01:39 -0400

    set up sign up form without functionality

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -4,7 +4,20 @@ function Signup() {
 
   return (
     <div className="Signup">
-      "Sign up"
+      <h1>Sign up</h1>
+      <p>If you don't have a Codewars account, click <a href="https://www.codewars.com/">here</a> to sign up!</p>
+      <p>Please enter your Codewars username below.</p>
+      <form>
+        <label>Codewars username: </label>
+        <input
+          type="text"
+          id="username"
+          required
+          // value={null}
+          onChange={handleUsernameChange}
+        />
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   )
 }

e2089d4de1ff6ef70f6e899d633d9f0b19437df2
Dana <dabinkim807@gmail.com>
2023-05-16 17:32:42 -0400

    set up validation form and instructions without functionality

--- client/src/components/Validation.jsx
+++ client/src/components/Validation.jsx
@@ -4,7 +4,13 @@ function Validation() {
 
   return (
     <div className="Validation">
-      "Validation"
+      <h1>Validation</h1>
+      <p>Thank you for signing up to CodeBook!</p> 
+      <p>To confirm your account, please complete the Codewars challenge below within 10 minutes.</p>
+      <p>Click on the "DONE" button once you've passed the challenge!</p>
+      {/* `https://www.codewars.com/kata/${user.test_challenge}` */}
+      <a href="">Code Challenge</a>
+      <button type="submit" onClick={handleDone}>Done</button>
     </div>
   )
 }

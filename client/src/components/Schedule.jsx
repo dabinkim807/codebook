5f9e76480b9c3468a07bf1f3111bc91124e1f058
Dana <dabinkim807@gmail.com>
2023-05-16 18:21:46 -0400

    set up schedule form without functionality

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -4,7 +4,77 @@ function Schedule() {
 
   return (
     <div className="Schedule">
-      "Schedule"
+      <h1>Schedule</h1>
+      <form>
+        <h2>Code Challenge Preferences</h2>
+
+        <label htmlFor="cc_rank">Difficulty</label>
+        <select name="cc_rank" id="cc_rank">
+          <option value="Beginner">Beginner</option>
+          <option value="Intermediate">Intermediate</option>
+          <option value="Professional">Professional</option>
+          <option value="Expert">Expert</option>
+        </select>
+
+        <label htmlFor="cc_category">Category</label>
+        <select name="cc_category" id="cc_category">
+          <option value="Algorithms">Algorithms</option>
+          <option value="Data Structures">Data Structures</option>
+        </select>
+
+        <label htmlFor="cc_frequency">Frequency</label>
+        <select name="cc_frequency" id="cc_frequency">
+          <option value="Every Week">Every Week</option>
+        </select>
+
+        <label htmlFor="cc_day">Day</label>
+        <select name="cc_day" id="cc_day">
+          <option value="Sunday">Sunday</option>
+          <option value="Monday">Monday</option>
+          <option value="Tuesday">Tuesday</option>
+          <option value="Wednesday">Wednesday</option>
+          <option value="Thursday">Thursday</option>
+          <option value="Friday">Friday</option>
+          <option value="Saturday">Saturday</option>
+        </select>
+
+
+        <h2>Email Preferences</h2>
+
+        <fieldset>
+          <legend>Reminders</legend>
+          <div>
+            <input
+              type="radio"
+              id="true"
+              name="e_reminder"
+              required
+              value="true"
+              onChange={handleReminderChange}
+            />
+            <label htmlFor="true">Yes</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="false"
+              name="e_reminder"
+              required
+              value="false"
+              onChange={handleReminderChange}
+            />
+            <label htmlFor="false">No</label>
+          </div>
+        </fieldset>
+
+        <label htmlFor="e_frequency">Frequency</label>
+        <select name="e_frequency" id="e_frequency">
+          <option value="Every Day">Every Day</option>
+        </select>
+
+        <button type="reset" onClick={handleReset}>Reset</button>
+        <button type="submit" onClick={handleSchedule}>Schedule</button>
+      </form>
     </div>
   )
 }

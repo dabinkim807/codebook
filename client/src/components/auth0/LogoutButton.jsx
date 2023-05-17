d820f76ac797c64356a6f9a8b0c9ffebe73d8777
Dana <dabinkim807@gmail.com>
2023-05-17 18:46:39 -0400

    import auth0 to auth0 components

--- client/src/components/auth0/LogoutButton.jsx
+++ client/src/components/auth0/LogoutButton.jsx
@@ -1,3 +1,6 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+
 function LogoutButton() {
   const { logout } = useAuth0();
 
--- client/src/components/auth0/Profile.jsx
+++ client/src/components/auth0/Profile.jsx
@@ -1,3 +1,6 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+
 function Profile() {
   const { user, isAuthenticated, isLoading } = useAuth0();
   

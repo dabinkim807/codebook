25bc20d8618e1ebe77ee70bb656b41815c0550dd
Dana <dabinkim807@gmail.com>
2023-05-17 17:36:56 -0400

    add Profile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,25 +8,6 @@ import Schedule from './components/Schedule';
 import Signup from './components/Signup';
 import MyNavBar from './components/Navbar';
 
-// const Profile = () => {
-//   const { user, isAuthenticated, isLoading } = useAuth0();
-  
-//   // my frontend has access to user data returned from Auth0; I can send the data to the backend
-//   console.log(user);
-//   if (isLoading) {
-//     return <div>Loading ...</div>;
-//   }
-
-//   return (
-//     isAuthenticated && (
-//       <div>
-//         <img src={user.picture} alt={user.name} />
-//         <h2>{user.name}</h2>
-//         <p>{user.email}</p>
-//       </div>
-//     )
-//   );
-// };
 
 function App() {
 //   const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
---
+++ client/src/components/auth0/Profile.jsx
@@ -0,0 +1,21 @@
+function Profile() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  
+  // my frontend has access to user data returned from Auth0; I can send the data to the backend
+  console.log(user);
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
+
+export default Profile

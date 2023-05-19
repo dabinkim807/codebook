35653dde6cf21a8abbbf2129ab399d6997de4255
Dana <dabinkim807@gmail.com>
2023-05-19 10:13:39 -0400

    add Landing test

---
+++ client/src/components/Landing.test.jsx
@@ -0,0 +1,9 @@
+import Landing from './Landing';
+import {expect, test} from 'vitest';
+import {render, screen} from '@testing-library/react';
+
+test('Landing renders correctly', () => {
+  const { getByTestId } = render(<Landing />);
+  const landingElement = getByTestId('landing');
+  expect(landingElement).toBeDefined();
+});
